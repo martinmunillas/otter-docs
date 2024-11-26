073e5d48b1b1e764c96bec02158a4d508afcb5e0
Martin Munilla <martinmunillas@gmail.com>
2024-11-25 16:50:02 -0800

    chore: use git hash

--- envs/envs.go
+++ envs/envs.go
@@ -2,12 +2,14 @@ package envs
 
 import (
 	"os"
+	"os/exec"
 )
 
-var Etag = os.Getenv("ETAG")
+var Etag string
 
 func init() {
-	if os.Getenv("IGNORE_CACHE") == "true" {
-		Etag = ""
+	if os.Getenv("IGNORE_CACHE") != "true" {
+		hash, _ := exec.Command("git", "rev-parse", "HEAD").Output()
+		Etag = string(hash)
 	}
 }
